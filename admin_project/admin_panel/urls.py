fea9261f01e99c5786987ea905f6ea7fa4bc7bd3
Vitaliy <fvetal19@gmail.com>
2025-02-16 12:41:26 +0200

    Task; Added new html admin page.

--- admin_project/admin_panel/urls.py
+++ admin_project/admin_panel/urls.py
@@ -1,8 +1,8 @@
-# admin_project/ugs/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('explore/', views.explore, name='explore')
+    path('explore/', views.explore, name='explore'),
+    path("custom_page/", views.custom_admin_page, name='custom_admin_page'),
 ]
